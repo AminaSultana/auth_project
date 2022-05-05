7d1490d00ddf4ab0724946c728f1e1d7e54e231b
AminaSultana <lilisultana53@gmail.com>
2022-05-05 12:23:38 +0530

    task 15 ecommerce

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -5,7 +5,7 @@ import classes from "./AuthForm.module.css";
 const AuthForm = () => {
   const emailInputRef = useRef();
   const passwordInputRef = useRef();
-  const [state, setstate] = useState("Create new account");
+  const [isLoading, setIsLoading] = useState(false);
 
   const [isLogin, setIsLogin] = useState(true);
 
@@ -13,46 +13,49 @@ const AuthForm = () => {
     setIsLogin((prevState) => !prevState);
   };
 
-  const submitHandler = async(event) => {
+  const submitHandler = async (event) => {
+    setIsLoading(true);
     event.preventDefault();
     const enteredEmail = emailInputRef.current.value;
     const enteredPassword = passwordInputRef.current.value;
-    try {
-      if (isLogin) {
-      } 
-      
-      else {
-        setstate("sending request")
-        const response = await fetch(
-          "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyB9epljL9fTX6GhIFodJAIjI89cSFFFElM",
-          {
-            method: "POST",
-            body: JSON.stringify({
-              email: enteredEmail,
-              password: enteredPassword,
-              returnSecureToken: true,
-            }),
-            headers: {
-              "Content-Type": "application/json",
-            },
-          }
-          )
-        if(response.ok){
-          alert("User added successfully") 
-          setstate("Create new account") 
-        }
-        else{
-          const data = await response.json()
-          throw new Error(data.error.message);
-        }
-      }
-      
-    } catch (error) {
-      alert(error)
-      setstate("Create new account")
+    let url;
+    if (isLogin) {
+      url =
+        "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyB9epljL9fTX6GhIFodJAIjI89cSFFFElM";
+    } else {
+      url =
+        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyB9epljL9fTX6GhIFodJAIjI89cSFFFElM";
     }
+    try {
+      const response = await fetch(url, {
+        method: "POST",
+        body: JSON.stringify({
+          email: enteredEmail,
+          password: enteredPassword,
+          returnSecureToken: true,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      if(response.ok){
+        const data = await response.json();
+        console.log(data.idToken);
+      }
+      else{
+        const data = await response.json();
+        throw new Error(data.error.message)
+      }
+    } catch (error) {
+      alert(error);
+    }
+    setIsLoading(false);
   };
 
+  let content = <p>Create New User</p>
+  if(isLoading){
+    content="Sending request";
+  }
   return (
     <section className={classes.auth}>
       <h1>{isLogin ? "Login" : "Sign Up"}</h1>
@@ -71,7 +74,7 @@ const AuthForm = () => {
           />
         </div>
         <div className={classes.actions}>
-          <button>{isLogin ? "Login" : state}</button>
+          <button>{isLogin ? "Login" : content}</button>
           <button
             type="button"
             className={classes.toggle}
